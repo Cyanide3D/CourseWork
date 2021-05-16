dd3c95767c4e433139be779420ecdcc5f12d2edf
Cyanide3D <gamer1997235@yandex.ru>
2021-05-16 20:11:49 +0300

    +fix

--- src/main/java/com/example/CourseWork/controller/IndexController.java
+++ src/main/java/com/example/CourseWork/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.CourseWork.controller;
 
+import com.example.CourseWork.models.Product;
 import com.example.CourseWork.models.User;
 import com.example.CourseWork.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -17,7 +20,10 @@ public class IndexController {
     @GetMapping
     public String index(Model model, @AuthenticationPrincipal User user) {
         model.addAttribute("user", user);
-        model.addAttribute("products", productService.findAllTradableProducts());
+        List<Product> prod = user != null
+                ? user.getRole().equalsIgnoreCase("ADMIN") ? productService.findAllProducts() : productService.findAllTradableProducts()
+                : productService.findAllTradableProducts();
+        model.addAttribute("products", prod);
         return "index";
     }
 
